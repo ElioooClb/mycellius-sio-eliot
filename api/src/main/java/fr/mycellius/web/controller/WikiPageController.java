7d27b3321911fe0068e9e26b459066d5a4347e1b
ElioooClb <collomb.eliot@gmail.com>
2026-01-27 12:59:21 +0100

    feat: séance 6

--- api/src/main/java/fr/mycellius/web/controller/WikiPageController.java
+++ api/src/main/java/fr/mycellius/web/controller/WikiPageController.java
@@ -1,10 +1,16 @@
-package fr.mycellius.web;
+package fr.mycellius.web.controller;
+import fr.mycellius.domain.Tag;
 import fr.mycellius.domain.WikiPage;
 import fr.mycellius.service.WikiService;
+import fr.mycellius.web.CreatePageRequest;
+import fr.mycellius.web.dto.TagRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import fr.mycellius.web.dto.CreateWikiPageRequest;
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/pages")
 public class WikiPageController {
@@ -13,12 +19,13 @@ public class WikiPageController {
         this.wikiService = wikiService;
     }
     @PostMapping
-    public ResponseEntity<WikiPage> createPage(@RequestBody CreatePageRequest request) {
+    public ResponseEntity<WikiPage> createPage(@Valid @RequestBody CreateWikiPageRequest request) {
+        List<Tag> domainTags = toDomainTags(request.tags());
         WikiPage page = wikiService.createPage(
                 request.id(),
                 request.title(),
                 request.content(),
-                request.tags()
+                domainTags
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(page);
     }
@@ -30,5 +37,12 @@ public class WikiPageController {
     @GetMapping("/search")
     public List<WikiPage> searchByTitle(@RequestParam("title") String fragment) {
         return wikiService.searchByTitle(fragment);
+    }
+    private List<Tag> toDomainTags(List<TagRequest> tags) {
+        if (tags == null) return List.of();
+        return tags.stream()
+                .map(TagRequest::name)
+                .map(Tag::new)
+                .toList();
     }
 }

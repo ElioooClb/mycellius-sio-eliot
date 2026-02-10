a9361a50972c73aae30f6b174fa601656ec95082
ElioooClb <collomb.eliot@gmail.com>
2026-02-10 10:00:02 +0100

    feat(api): jwt auth + rbac + seed users + cors

---
+++ api/src/main/java/fr/mycellius/web/dto/auth/LoginRequest.java
@@ -0,0 +1,2 @@
+package fr.mycellius.web.dto.auth;
+public record LoginRequest(String username, String password) {}
---
+++ api/src/main/java/fr/mycellius/web/dto/auth/LoginResponse.java
@@ -0,0 +1,2 @@
+package fr.mycellius.web.dto.auth;
+public record LoginResponse(String token, String username, String role) {}
